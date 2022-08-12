74b4dd4c06c25160f0b78f9cfe4b6598d9d88d39
Shaban <shabanxhemajli@live.com>
2022-08-12 14:51:06 +0200

    dd56

--- exercise (6).py
+++ exercise (6).py
@@ -1,9 +1,20 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
-#Task 1
+'''#Task 1
 a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 c= int(input("Enter second number:"))
 sum = a+b+c 
-print("The sum of the three numbers is:"+ str(sum))
+print("The tripple sum is:"+ str(sum))'''
+#Task2
+for i in range(3):
+    a =int(input("First number "))
+    b = int(input("Second number "))
+    if a > b:
+        diff = a - b
+    else:
+        diff = b - a
+
+    print("The result of the calculation is:" + str(diff))
+
