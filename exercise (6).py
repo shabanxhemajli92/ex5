c7c537365078a6aa12abc2278b6c812b2c487fe3
Shaban <shabanxhemajli@live.com>
2022-08-12 14:13:08 +0200

    first commit

---
+++ exercise (6).py
@@ -0,0 +1,9 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+#Task 1
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c= int(input("Enter second number:"))
+sum = a+b+c 
+print("The sum of the three numbers is:"+ str(sum))
